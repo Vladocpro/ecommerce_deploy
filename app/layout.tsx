62a5212523c509b493fffce85c25cf50fadaac8d
vladocpro <infernohs9@gmail.com>
2023-07-08 14:37:17 +0100

    components

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,8 @@ import Providers from "./components/Provider";
 import AuthPopup from "./components/modals/AuthPopup";
 import Toast from "./components/ToastNotification";
 import SelectSizePopup from "./components/modals/SelectSizePopup";
-import {Inter} from "next/dist/compiled/@next/font/dist/google";
 import head from "./head";
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
