15a8ad88a8798df37bcaf2740919950533b4d87c
vladocpro <infernohs9@gmail.com>
2023-07-08 14:58:26 +0100

    api: user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,17 +1,17 @@
-import getCurrentUser from "../../actions/getCurrentUser";
-import {NextResponse} from "next/server";
-
-export async function GET() {
-
-   try {
-      const currentUser  = await getCurrentUser();
-      if(currentUser === null) {
-         return NextResponse.error();
-      }
-      return NextResponse.json(currentUser)
-
-   } catch (e) {
-      console.log(e)
-   }
-
-}
+// import getCurrentUser from "../../actions/getCurrentUser";
+// import {NextResponse} from "next/server";
+//
+// export async function GET() {
+//
+//    try {
+//       const currentUser  = await getCurrentUser();
+//       if(currentUser === null) {
+//          return NextResponse.error();
+//       }
+//       return NextResponse.json(currentUser)
+//
+//    } catch (e) {
+//       console.log(e)
+//    }
+//
+// }
